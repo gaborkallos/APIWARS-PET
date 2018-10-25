32c1b7a99b2b39b0aa519ed028e653caf7593db8
gaborKriger <regirck@gmail.com>
2018-10-25 14:52:47 +0200

    count child

--- static/javascript/resident.js
+++ static/javascript/resident.js
@@ -1,13 +1,33 @@
 function reply_click(clickedID) {
     let residents = clickedID.split(",");
-
     for (let i = 0; i < residents.length; i++) {
-        $.getJSON(residents[i], function (response) {
-
-            console.log(response);
-        });
+        getResident(residents[i]);
     }
 }
+
+function getResident(residents) {
+    $.getJSON(residents, function (response) {
+        let resident = response;
+        let allResident = document.getElementById('residentmodal');
+        let newResident = document.getElementById('residentmodal').innerHTML;
+        newResident = newResident.replace('@Name@', resident['name']);
+        allResident.closest('.modal-table').innerHTML += newResident;
+    });
+}
+
+let closeModal = document.getElementById("modalclose");
+closeModal.addEventListener('click', function () {
+
+    var countPlanets = document.getElementById("planet-table").childElementCount;
+
+    console.log(countPlanets);
+
+    let modalList = document.getElementsByTagName('tbody')[countPlanets];
+    console.log(modalList);
+
+    modalList.innerHTML = "";
+
+});
 
 
 
